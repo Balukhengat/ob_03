2ce8e2cda6a2a260555a9f2a116e326e9f36bfc3
abs <abhijitkumbhar001@gmail.com>
2016-10-24 11:44:18 +0530

    profile

--- application/views/pages/profile/profile_container.php
+++ application/views/pages/profile/profile_container.php
@@ -8,15 +8,7 @@
 						 	Lorem ipsum dolor sit amet, wisi tincidunt in per, te sed partem quidam. Ne eum causae placerat posidonium, mei no molestie temporibus. Corpora interesset definitionem in his, tota tacimates inimicus eam ex. Te nulla voluptatum consectetuer sed. Justo euismod ocurreret in qui, cum populo deterruisset et.
 						 	</p>
 						 </div>
-						 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-							<div class="well"><a href="">My profile</a></div>
-							<div class="well"><a href="">Dashboard</a></div>
-							<div class="well"><a href="">RealEstate</a></div>
-							<div class="well"><a href="">Tution</a></div>
-							<div class="well"><a href="">Hotels and resturents</a></div>
-							<div class="well"><a href="">Travelling</a></div>
-							<div class="well"><a href="">automobile</a></div>		
-						 </div>
+						 <?php $this->load->view('pages/profile/profilemenu')?>
 					</div>	
 				</div>
 			</div>
---
+++ application/views/pages/profile/profilemenu.php
@@ -0,0 +1,21 @@
+<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+							<div class="well" id="menuhover"><a href="">My profile</a></div>
+							<div class="well" id="menuhover1"><a href="">Dashboard</a></div>
+							<div class="well" id="menuhover2"><a href="">RealEstate</a></div>
+							<div class="well" id="menuhover3"><a href="">Tution</a></div>
+							<div class="well" id="menuhover4"><a href="">Hotels and resturents</a></div>
+							<div class="well" id="menuhover5"><a href="">Travelling</a></div>
+							<div class="well" id="menuhover6"><a href="">automobile</a></div>		
+							<div class="well" id="menuhover7"><a href="">other</a></div>		
+</div>
+<script type="text/javascript">
+$(".well").mouseenter(
+  function(){
+    $(this).css('background-color','#d72c31');
+  });
+
+ $(".well").mouseleave(
+  function(){
+    $(this).css('background-color','#f5f5f5');
+  });
+</script>
