e0c7f1f37fb4a47e37aa773f67f3aa20ee29d47e
abs <abhijitkumbhar001@gmail.com>
2016-10-24 10:45:50 +0530

    a

---
+++ application/views/pages/profile/profile.php
@@ -0,0 +1,11 @@
+<html>
+	<?php $this->load->view('layout/head');?>
+	<body>
+		<?php $this->load->view('layout/leftsidebar.php');?>
+		<?php $this->load->view('layout/header.php');?>
+		<?php $this->load->view('layout/category.php');?>
+		<?php $this->load->view('pages/profile/profile_container')?>
+		<?php $this->load->view('layout/footer.php');?>
+		<?php $this->load->view('layout/js.php');?>
+	</body>
+</html>
