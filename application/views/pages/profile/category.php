b9d80f09969a982daeb4e59b7bd466b1f700232c
Balukhengat <balu.khengat@gmail.com>
2016-11-09 14:44:13 +0530

    ukluklu

--- application/views/pages/profile/category.php
+++ application/views/pages/profile/category.php
@@ -2,15 +2,15 @@
 	<div class="row">
 	<div class="category-header-right">
 	<ul>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_profile">My profile</a></li>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_dashboard">Dashboard</a></li>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_realestate">RealEstate</a></li>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_tution">Tution</a></li>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_hotel">Hotels & Restaurents</a></li>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_travelling">Travelling</a></li>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_automobile">Automobile</a></li>
-									<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_other">Other</a></li>
-								</ul>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_profile">My profile</a></li>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_dashboard">Dashboard</a></li>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_realestate">RealEstate</a></li>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_tution">Tution</a></li>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_hotel">Hotels & Restaurents</a></li>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_travelling">Travelling</a></li>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_automobile">Automobile</a></li>
+		<li class="dropdown head-dpdn"><a href="<?php echo base_url();?>Basic_controller/user_other">Other</a></li>
+	</ul>
 	</div>
 	</div>
 </div>
--- application/views/pages/profile/dashboard.php
+++ application/views/pages/profile/dashboard.php
@@ -1,3 +1,12 @@
+<?php
+$userid = $_SESSION['userid'];
+		$this->db->select('t1.name, t2.name,t1.category,t2.category')
+          ->from('realestate AS t1, tution AS t2')
+          ->where('t1.userid = t2.userid');
+	$dashboard_info=$query = $this->db->get()->result_array();
+	print_r($dashboard_info);die();
+		?>
+		
 <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
 	<table class="table table-hover">
     <thead>
@@ -9,12 +18,26 @@
       </tr>
     </thead>
     <tbody>
+    <?php foreach ($dashboard_info as $row){
+?>
       <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>Real estate</td>
+        <td><?php echo $row['name'];?></td>
+        <td><?php echo $row['title'];?></td>
+        <td><?php $category=$row['category'];
+       	 		if($category=='0'){
+        			echo"Realestate";}
+        		elseif($category=='1'){
+        			echo"Tution";}
+        		elseif($category=='2'){
+        			echo"Hotels $ Restaurents";}
+        		elseif($category=='3'){
+       				echo"Travelling";}
+       			elseif($category=='4'){
+       				echo"Automobiles";}
+        ?></td>
         <td><button class="btn btn-success btn-sm">Edit</button>&nbsp;<button class="btn btn-danger btn-sm">Delete</button></td>
       </tr>
+      <?php }?>
     </tbody>
   </table>		
 </div>
